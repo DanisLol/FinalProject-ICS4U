dd5a5b70c2859aeae68f17a71bb4deaf6558be42
angxw <angelawang408@gmail.com>
2025-06-12 11:19:08 -0400

    Update HurtableEntity.java

--- HurtableEntity.java
+++ HurtableEntity.java
@@ -8,9 +8,38 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public abstract class HurtableEntity extends Scroller
 {
+    protected GreenfootImage spritesheet, spritesheetLarge;
+    protected Animation walkAnimation, deathAnimation, attackAnimation, curAnimation = walkAnimation;
+    protected int countdown, direction, frame = 0;
+    protected int highestIndex;
+    protected ActionState curAction = ActionState.NOTHING, lastAction = ActionState.NOTHING;
+
     protected int health;
     
+    public void animate(){
+        if (countdown > 0){
+            countdown--;
+        } else {
+            setImage (curAnimation.getOneImage(Direction.fromInteger(direction), frame));
+
+            frame++;
+            if (frame > highestIndex){
+                if (curAction == ActionState.WALKING) {
+                    frame = 1;
+                } else {
+                    frame = 0; 
+                    curAction = ActionState.NOTHING; //change????? 
+                    lastAction = ActionState.ATTACKING; 
+                    //why does this not work properly omds OH
+                    curAnimation = walkAnimation;
+                    //setImage(walkAnimation.getOneImage(Direction.fromInteger(direction), frame));
+                }
+            }
+            countdown = 6;
+        }
+    }
+
     public abstract void takeDamage(int dmg);
-    
+
     public abstract void die();
 }
